d59fed4c55669c29c21b09aebf081566a8567d5e
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2024-12-27 19:44:31 +0100

    Create shops repository

---
+++ Assets/_Scripts/GameCode/Persistence/Repositories/Shops.cs
@@ -0,0 +1,11 @@
+using System;
+using GameCode.Persistence.Models;
+
+namespace GameCode.Persistence.Repositories
+{
+    [Serializable]
+    public class Shops : Repository<Shop>
+    {
+        
+    }
+}
