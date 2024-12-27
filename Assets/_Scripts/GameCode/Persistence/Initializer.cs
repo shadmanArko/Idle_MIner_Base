a3c236bb51dad171539f7085385e564f31433085
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2024-12-27 21:35:53 +0100

    Create Initializer

---
+++ Assets/_Scripts/GameCode/Persistence/Initializer.cs
@@ -0,0 +1,55 @@
+using System.IO;
+using System.Threading.Tasks;
+using GameCode.Persistence.Repositories;
+using UnityEngine;
+
+namespace GameCode.Persistence
+{
+    public class Initializer : MonoBehaviour
+    {
+        public DataContext context;
+
+        [Header("Repositories")] 
+        public Shops shops;
+        
+        [Header("Other Dependencies")] 
+        public TextAsset saveDataJsonFile;
+
+        private static string SaveDataFilePath => $"{Application.persistentDataPath}/Saves/save_data.json";
+
+        private async void Start()
+        {
+            await LoadDataAsync();
+        }
+
+        private async Task LoadDataAsync()
+        {
+            Debug.Log(Application.persistentDataPath);
+            var rootSaveDirectory = Path.Combine(Application.persistentDataPath, "Saves");
+            if (!Directory.Exists(rootSaveDirectory))
+            {
+                Directory.CreateDirectory(rootSaveDirectory);
+            }
+            
+            if (!File.Exists(SaveDataFilePath))
+            {
+                await BootstrapSaveData();
+            }
+            
+            await context.Load();
+
+            BindContexts();
+        }
+
+        private void BindContexts()
+        {
+            shops.context = context;
+        }
+
+        private async Task BootstrapSaveData()
+        {
+            using var writer = new StreamWriter(SaveDataFilePath);
+            await writer.WriteAsync(saveDataJsonFile.text);
+        }
+    }
+}
